c0f9c4492a62622f25f6d2c220775441e33e0a96
Johnathan <j88ks6mz7q@snkmail.com>
2019-08-26 12:06:42 +0200

    asd

--- vpc/subnetwork/main.tf
+++ vpc/subnetwork/main.tf
@@ -1,3 +1,8 @@
+terraform {
+  # Intentionally empty. Will be filled by Terragrunt.
+  backend "gcs" {}
+}
+
 resource "google_compute_subnetwork" "vpc_subnetwork" {
   name          = var.vpc_subnetwork_name
   ip_cidr_range = var.vpc_subnetwork_cidr
