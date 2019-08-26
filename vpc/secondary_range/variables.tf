8ec6ed37a76e0cefbe564abe5fcad6f4c0575760
Johnathan <j88ks6mz7q@snkmail.com>
2019-08-26 12:09:15 +0200

    asd

---
+++ vpc/secondary_range/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+}
+
+variable "vpc_subnetwork_name" {
+  description = "Name of VPC"
+  type        = string
+}
+
+variable "vpc_subnetwork_cidr" {
+  description = "Name of VPC"
+  type        = string
+}
+
+variable "vpc_subnetwork_secondary_name" {
+  description = "Name of VPC"
+  type        = string
+}
+
+variable "vpc_subnetwork_secondary_cidr" {
+  description = "Name of VPC"
+  type        = string
+}
+
+variable "vpc_subnetwork_region" {
+  description = "Name of VPC"
+  type        = string
+}
